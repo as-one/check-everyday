b7ebcad57d121ef5b553479a26e6156df2869718
Cesar Kohl <cesarkohl@gmail.com>
2020-08-06 10:44:47 -0300

    Build docs.

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,7 @@
-import loadHtml from '../../node_modules/@as-one/components/libs/FetchHtml/FetchHtml.js';
-import Router from '../../node_modules/@as-one/components/libs/Router/Router.js';
+// import loadHtml from '../../node_modules/@as-one/components/libs/FetchHtml/FetchHtml.js';
+// import Router from '../../node_modules/@as-one/components/libs/Router/Router.js';
+import loadHtml from './libs/FetchHtml.js';
+import Router from './libs/Router.js';
 
 const router = new Router({
   mode: 'hash',
