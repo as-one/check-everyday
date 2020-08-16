6773b19d90c0cebfe710a30dff7c7c3dd4463d71
Cesar Kohl <cesarkohl@gmail.com>
2020-08-16 20:55:30 -0300

    Update docs. Minor changes.

---
+++ docs/app/index.js
@@ -0,0 +1,15 @@
+import loadHtml from "./libs/FetchHtml.js";
+import Router from "./libs/Router.js";
+const router = new Router({
+  mode: "hash",
+  root: "/"
+});
+router.add(/html/, () => {
+  loadHtml(".content", "app/views/html.html");
+}).add(/css/, () => {
+  loadHtml(".content", "app/views/css.html");
+}).add(/js/, () => {
+  loadHtml(".content", "app/views/js.html");
+}).add("", () => {
+  loadHtml(".content", "app/views/html.html");
+});
