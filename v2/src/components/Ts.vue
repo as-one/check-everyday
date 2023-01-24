b0ab67e24ec4b2b53d447088fee360fa6583cad8
Juliana Cochenski <juliana.cochenski@gmail.com>
2023-01-24 11:26:31 -0300

    Improve Ts.vue CSS

--- v2/src/components/Ts.vue
+++ v2/src/components/Ts.vue
@@ -21,10 +21,10 @@
         </div>
         <div class="row">
           <div class="col-sm-12">
-            <div class="pre">
-              let varUnknown: unknown;
-              let varAny: any;
-            </div>
+            <pre class="pre">
+let varUnknown: unknown;
+let varAny: any;
+            </pre>
           </div>
         </div>
 
@@ -35,12 +35,12 @@
         </div>
         <div class="row">
           <div class="col-sm-12">
-            <div class="pre">
-              let varString: string = "Hello World";
-              let varBoolean: boolean = true;
-              let varNumber: number = 10;
-              let varArray: string[] = ['one', 'two', 'three'];
-            </div>
+            <pre class="pre">
+let varString: string = "Hello World";
+let varBoolean: boolean = true;
+let varNumber: number = 10;
+let varArray: string[] = ['one', 'two', 'three'];
+            </pre>
           </div>
         </div>
 
@@ -51,16 +51,16 @@
         </div>
         <div class="row">
           <div class="col-sm-12">
-            <div class="pre">
-              let varObject: {
-              one: string;
-              two?: boolean; // optional
-              callback(): void;
-              } = {
-              one: 'one',
-              callback: () => { }
-              };
-            </div>
+            <pre class="pre">
+let varObject: {
+  one: string;
+  two?: boolean; // optional
+  callback(): void;
+} = {
+  one: 'one',
+  callback: () => { }
+};
+            </pre>
           </div>
         </div>
 
@@ -71,16 +71,16 @@
         </div>
         <div class="row">
           <div class="col-sm-12">
-            <div class="pre">
-              interface IVarObject {
-              one: string;
-              two?: boolean; // optional
-              }
+            <pre class="pre">
+interface IVarObject {
+  one: string;
+  two?: boolean; // optional
+}
 
-              let varObject: IVarObject = {
-              one: 'one'
-              };
-            </div>
+let varObject: IVarObject = {
+  one: 'one'
+};
+            </pre>
           </div>
         </div>
       </div>
@@ -98,13 +98,13 @@
         </div>
         <div class="row">
           <div class="col-sm-12">
-            <div class="pre">
-              function consoleLogMessage(message: any): void {
-              console.log(message);
-              }
+            <pre class="pre">
+function consoleLogMessage(message: any): void {
+  console.log(message);
+}
 
-              consoleLogMessage('Message'); // Message
-            </div>
+consoleLogMessage('Message'); // Message
+            </pre>
           </div>
         </div>
 
@@ -115,13 +115,13 @@
         </div>
         <div class="row">
           <div class="col-sm-12">
-            <div class="pre">
-              function add(x: number, y: number): number {
-              return x + y;
-              }
+            <pre class="pre">
+function add(x: number, y: number): number {
+  return x + y;
+}
 
-              add(1, 2); // 3
-            </div>
+add(1, 2); // 3
+            </pre>
           </div>
         </div>
 
@@ -132,13 +132,13 @@
         </div>
         <div class="row">
           <div class="col-sm-12">
-            <div class="pre">
-              function addFromObject({ x, y }: { x: number; y: number }): number {
-              return x + y;
-              }
+            <pre class="pre">
+function addFromObject({ x, y }: { x: number; y: number }): number {
+  return x + y;
+}
 
-              addFromObject({ x: 1, y: 2 }); // 3
-            </div>
+addFromObject({ x: 1, y: 2 }); // 3
+            </pre>
           </div>
         </div>
       </div>
@@ -163,20 +163,20 @@
             <h4>null & undefined</h4>
           </div>
         </div>
-        <div class="pre">
-          let varNull: null = null;     // useless
-          let varUndefined: undefined;  // useless
-        </div>
+        <pre class="pre">
+let varNull: null = null;     // useless
+let varUndefined: undefined;  // useless
+        </pre>
 
         <div class="row">
           <div class="col-sm-12">
             <h4>unknown & any</h4>
           </div>
         </div>
-        <div class="pre">
-          let varArray: unknown[] = ['one', true, 3];
-          let varArray: any[] = ['one', true, 3];
-        </div>
+        <pre class="pre">
+let varArray: unknown[] = ['one', true, 3];
+let varArray: any[] = ['one', true, 3];
+        </pre>
       </div>
 
       <div class="col-sm-12 col-md-6">
@@ -191,33 +191,33 @@
             <h4>function is type object</h4>
           </div>
         </div>
-        <div class="pre">
-          let varFunction: {} = () => { };
-        </div>
+        <pre class="pre">
+let varFunction: {} = () => { };
+        </pre>
 
         <div class="row">
           <div class="col-sm-12">
             <h4>function with array param</h4>
           </div>
         </div>
-        <div class="pre">
-          let varFunctionArray = function (varArray: string[]) {
-          return varArray;
-          }
+        <pre class="pre">
+let varFunctionArray = function (varArray: string[]) {
+  return varArray;
+}
 
-          varFunctionArray(['one', 'two', 'three']);
-        </div>
+varFunctionArray(['one', 'two', 'three']);
+        </pre>
 
         <div class="row">
           <div class="col-sm-12">
             <h4>function with object param</h4>
           </div>
         </div>
-        <div class="pre">
-          let varFunctionObject = function (param1: string, param2: {}) { }
+        <pre class="pre">
+let varFunctionObject = function (param1: string, param2: {}) { }
 
-          varFunctionObject('one', { one: 'one', two: true, three: 3 });
-        </div>
+varFunctionObject('one', { one: 'one', two: true, three: 3 });
+        </pre>
       </div>
 
     </div>
@@ -236,83 +236,83 @@
                 <h4>with interface</h4>
               </div>
             </div>
-            <div class="pre">
-              interface AClass {
-              varOne: string;
-              varTwo: boolean;
-              varThree?: number;
-              }
+            <pre class="pre">
+interface AClass {
+  varOne: string;
+  varTwo: boolean;
+  varThree?: number;
+}
 
-              class aClass implements AClass {
-              varOne = 'one';
-              varTwo = true;
-              }
-            </div>
+class aClass implements AClass {
+  varOne = 'one';
+  varTwo = true;
+}
+            </pre>
 
             <div class="row">
               <div class="col-sm-12">
                 <h4>extends and implements</h4>
               </div>
             </div>
-            <div class="pre">
-              interface OldClass {
-              varZero: number;
-              }
+            <pre class="pre">
+interface OldClass {
+  varZero: number;
+}
 
-              interface NewClass extends OldClass {
-              varOne: string;
-              varTwo: boolean;
-              varThree?: number;
-              }
+interface NewClass extends OldClass {
+  varOne: string;
+  varTwo: boolean;
+  varThree?: number;
+}
 
-              class newClass implements NewClass {
-              varZero = 0;
-              varOne = 'one';
-              varTwo = true;
-              }
-            </div>
+class newClass implements NewClass {
+  varZero = 0;
+  varOne = 'one';
+  varTwo = true;
+}
+            </pre>
 
             <div class="row">
               <div class="col-sm-12">
                 <h4>hybrid</h4>
               </div>
             </div>
-            <div class="pre">
-              interface IHybridClass {
-              varOne: string;
-              varTwo: {
-              twoOne: boolean;
-              };
-              varThree(varThreeOne: string, varThreeTwo: boolean): number;
-              varFour({ varFourOne, varFourTwo }: { varFourOne: string, varFourTwo: boolean }): number;
-              }
+            <pre class="pre">
+interface IHybridClass {
+  varOne: string;
+  varTwo: {
+    twoOne: boolean;
+  };
+  varThree(varThreeOne: string, varThreeTwo: boolean): number;
+  varFour({ varFourOne, varFourTwo }: { varFourOne: string, varFourTwo: boolean }): number;
+}
 
-              class HybridClass implements IHybridClass {
-              varOne = 'one';
+class HybridClass implements IHybridClass {
+  varOne = 'one';
 
-              varTwo = {
-              twoOne: true
-              };
+  varTwo = {
+    twoOne: true
+  };
 
-              varThree = (varThreeOne: string, varThreeTwo: boolean) => {
-              return 3;
-              };
-              testingVarThree() {
-              let three = this.varThree('one', true);
-              }
+  varThree = (varThreeOne: string, varThreeTwo: boolean) => {
+    return 3;
+  };
+  testingVarThree() {
+    let three = this.varThree('one', true);
+  }
 
-              varFour = function ({
-              varFourOne,
-              varFourTwo
-              }: { varFourOne: string, varFourTwo: boolean }) {
-              console.log(varFourOne, varFourTwo);
-              return 4;
-              }
-              testingVarFour() {
-              let four = this.varFour({ varFourOne: 'four', varFourTwo: true });
-              }
-              }
-            </div>
+  varFour = function ({
+    varFourOne,
+    varFourTwo
+  }: { varFourOne: string, varFourTwo: boolean }) {
+    console.log(varFourOne, varFourTwo);
+    return 4;
+  }
+  testingVarFour() {
+    let four = this.varFour({ varFourOne: 'four', varFourTwo: true });
+  }
+}
+            </pre>
           </div>
         </div>
       </div>
@@ -329,34 +329,34 @@
                 <h4>simple</h4>
               </div>
             </div>
-            <div class="pre">
-              function identity&lt;T>(arg: T): T {
-              return arg;
-              }
+            <pre class="pre">
+function identity&lt;T>(arg: T): T {
+  return arg;
+}
 
-              identity(10);
-              identity&lt;number>(10);
-              identity('ten');
-              identity&lt;string>('ten');
-            </div>
+identity(10);
+identity&lt;number>(10);
+identity('ten');
+identity&lt;string>('ten');
+            </pre>
 
             <div class="row">
               <div class="col-sm-12">
                 <h4>extends</h4>
               </div>
             </div>
-            <div class="pre">
-              interface ILoggingIdentity2 {
-              length: number;
-              }
+            <pre class="pre">
+interface ILoggingIdentity2 {
+  length: number;
+}
 
-              function loggingIdentity2&lt;T extends ILoggingIdentity2>(arg: T): T {
-              console.log(arg.length);
-              return arg;
-              }
+function loggingIdentity2&lt;T extends ILoggingIdentity2>(arg: T): T {
+  console.log(arg.length);
+  return arg;
+}
 
-              loggingIdentity2({ length: 1 });
-            </div>
+loggingIdentity2({ length: 1 });
+            </pre>
           </div>
         </div>
       </div>
@@ -371,9 +371,9 @@
         </div>
         <div class="row">
           <div class="col-sm-12">
-            <div class="pre">
-              // Later
-            </div>
+            <pre class="pre">
+// Later
+            </pre>
           </div>
         </div>
       </div>
@@ -385,11 +385,11 @@
         </div>
         <div class="row">
           <div class="col-sm-12">
-            <div class="pre">
-              // Later
-              Literal Types: ie. 'Value1' | 'Value2'
-              Union Types:   ie. boolean | string
-            </div>
+            <pre class="pre">
+// Later
+Literal Types: ie. 'Value1' | 'Value2'
+Union Types:   ie. boolean | string
+            </pre>
           </div>
         </div>
       </div>
@@ -411,24 +411,24 @@
 
     <div class="row">
       <div class="col-sm-12">
-        <div class="pre">
-          interface IButton {
-          text: string;
-          color?: string;
-          size?: number;
-          }
+        <pre class="pre">
+interface IButton {
+  text: string;
+  color?: string;
+  size?: number;
+}
 
-          function Button({ text, color, size }: IButton) {
-          // variables
-          // logic
-          // return &lt;button>{text}&lt;/button>;
-          }
+function Button({ text, color, size }: IButton) {
+  // variables
+  // logic
+  // return &lt;button>{text}&lt;/button>;
+}
 
-          export default Button;
+export default Button;
 
-          &lt;Button text="Click Here" />
-          &lt;Button text="Click Here" color="purple" size={10} />
-        </div>
+&lt;Button text="Click Here" />
+&lt;Button text="Click Here" color="purple" size={10} />
+        </pre>
       </div>
     </div>
   </div>
