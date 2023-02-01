3a0b910b4f572e8e261d7e95aa9d7d298b075aad
Juliana Cochenski <juliana.cochenski@gmail.com>
2023-02-01 13:24:14 -0300

    Move v2 to root

--- v1/docs/app/libs/FetchHtml.js
+++
@@ -1,8 +0,0 @@
-async function fetchHtmlAsText(t) {
-  const e = await fetch(t);
-  return await e.text();
-}
-
-export default async function loadHtml(t, e) {
-  document.querySelector(t).innerHTML = await fetchHtmlAsText(e);
-}
