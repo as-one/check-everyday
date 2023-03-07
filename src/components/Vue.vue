c6dc383fd71bdfe77460493d89e3fb107c688248
Juliana Cochenski <juliana.cochenski@gmail.com>
2023-03-07 09:29:04 -0300

    Improvement Vue page

--- src/components/Vue.vue
+++ src/components/Vue.vue
@@ -108,7 +108,298 @@ function handleClick() {
   emit('handleButton', 'Clicked!');
 }
 < /script>
-  `
+  `,
+  sharingStateBottomUpChildToGrandpa: `
+// Grandpa.vue
+
+< template>
+  <dad @handleButton="showAlert" />
+< /template>
+
+< script setup>
+import Dad from "@/Dad.vue";
+
+function showAlert(value) {
+  alert(value);
+};
+< /script>
+
+==========
+
+// Dad.vue
+
+< template>
+  <child />
+< /template>
+
+< script setup>
+import Child from "@/Child.vue";
+< /script>
+
+==========
+
+// Child.vue
+
+< template>
+  <button @click="$dad.$emit('handleButton', 'Clicked!')>Button</button>
+< /template>
+  `,
+  methodHandlers: `
+< template>
+// Inline
+<button @click="counter++">Add 1</button>
+<p>The button above has been clicked { { counter }} times.</p>
+
+// Function
+<button @click="greet">Greet</button>
+<button @click="say('hello')">Say Hello</button>
+< /template>
+
+< script setup>
+import { ref } from 'vue';
+
+const counter = ref(0);
+
+function greet(event) {
+  // event is the native DOM event
+  console.log(event.target.tagName);
+}
+
+function say(message) {
+  console.log(message);
+}
+< /script>
+  `,
+  methodEventModifiers: `
+< template>
+  <a @click.stop="doThis"></a>
+  <a @click.stop.prevent="doThat"></a>
+  <div @click.self="myFunction">...</div>
+  <form @submit.prevent="onSubmit"></form>
+< /template>
+  `,
+  methodKeyModifiers: `
+< template>
+  <input @keyup.enter="submit" />
+  <input @keyup.page-down="onPageDown" />
+  <input @keyup.delete="onDelete" /> // captures both "Delete" and "Backspace" keys
+< /template>
+  `,
+  conditionalVif: `
+< template v-if="ok">
+  <h1>Title</h1>
+  <p>Paragraph 1</p>
+  <p>Paragraph 2</p>
+< /template>
+  `,
+  conditionalVifElse: `
+< template>
+  <div v-if="type === 'A'">A</div>
+  <div v-else-if="type === 'B'">B</div>
+  <div v-else>Else</div>
+< /template>
+  `,
+  conditionalVshow: `
+< template>
+  <h1 v-show="ok">Hello!</h1>
+< /template>
+
+The v-show always renders in the DOM just toggling the CSS.
+  `,
+  listVforArray: `
+< template>
+  <li v-for="(item, index) in items" :key="item.message">
+    { { item.message }}
+  </li>
+< /template>
+
+< script setup>
+import { ref } from 'vue';
+
+const items = ref([
+  { message: 'Foo' },
+  { message: 'Bar' },
+]);
+< /script>
+  `,
+  listVforObject: `
+< template>
+   <ul>
+    <li v-for="(value, key, index) in myObject" :key="index">
+      { { index }}. { { key }}: { { value }}
+      // 0. title: My Title
+      // 1. author: My Author
+    </li>
+  </ul>
+< /template>
+
+< script setup>
+import { reactive } from 'vue';
+
+const myObject = reactive({
+  title: 'My Title',
+  author: 'My Author',
+});
+< /script>
+  `,
+  listVforComponent: `
+< my-component
+  v-for="(item, index) in items"
+  :item="item"
+  :index="index"
+  :key="item.id"
+/>
+  `,
+  templateRefsAccessing: `
+< template>
+  <input ref="input" />
+< /template>
+
+< script setup>
+import { ref, onMounted } from 'vue';
+
+const input = ref(null);
+
+onMounted(() => {
+  input.value.focus();
+});
+< /script>
+  `,
+  reactivityRef: `
+// Use for scalar types
+
+< script setup>
+import {ref} from 'vue';
+
+const title = ref('Title');
+const price = ref(9.99);
+
+console.log(title.value);
+console.log(price.value);
+< /script>
+  `,
+  reactivityReactive: `
+// Use for compound types
+
+< script setup>
+import {reactive} from 'vue';
+
+const product = reactive({
+  title: 'Title',
+  price: 9.99
+});
+
+console.log(product.title);
+< /script>
+  `,
+  reactivityRefState: `
+< template>
+  <button @click="increment">
+    { { count }}
+  </button>
+< /template>
+
+< script>
+import { ref } from 'vue';
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+< /script>
+  `,
+  reactivityReactiveState: `
+< template>
+  <button @click="increment">
+    { { state.count }}
+  </button>
+< /template>
+
+< script setup>
+import { reactive } from 'vue';
+
+const state = reactive({ count: 0 });
+
+function increment() {
+  state.count++
+};
+< /script>
+  `,
+  routing: `
+< template>
+  <a href="#/">Home</a>
+  <a href="#/about">About</a>
+  <a href="#/not-found">Not Found Link</a>
+  <component :is="currentView" />
+< /template>
+
+< script setup>
+import { ref, computed } from 'vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from './Home.vue';
+import About from './About.vue';
+import NotFound from './NotFound.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: About,
+    },
+    {
+      path: "/not-found",
+      name: "notFound",
+      component: NotFound,
+    },
+  ],
+});
+
+export default router;
+
+const currentPath = ref(window.location.hash);
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+});
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
+});
+
+< /script>
+
+==========
+App.vue
+
+< template>
+  <nav>
+    <RouterLink to="/home">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/not-found">NotFound</RouterLink>
+  </nav>
+< /template>
+
+< script setup>
+import { RouterLink, RouterView } from "vue-router";
+< /script>
+  `,
+  redirecting: `
+< template>< /template>
+
+< script setup>
+created() {
+  this.$router.push('/my-path');
+}
+< /script>
+  `,
 }
 </script>
 
@@ -158,40 +449,8 @@ function handleClick() {
 
         <h5>Child to Grandpa</h5>
         <pre class="pre">
-// Grandpa.vue
-
-&lt;template>
-  &lt;dad @handleButton="showAlert" />
-&lt;/template>
-
-&lt;script setup>
-import Dad from "@/Dad.vue";
-
-function showAlert(value) {
-  alert(value);
-};
-&lt;/script>
-
-==========
-
-// Dad.vue
-
-&lt;template>
-  &lt;child />
-&lt;/template>
-
-&lt;script setup>
-import Child from "@/Child.vue";
-&lt;/script>
-
-==========
-
-// Child.vue
-
-&lt;template>
-  &lt;button @click="$dad.$emit('handleButton', 'Clicked!')>Button&lt;/button>
-&lt;/template>
-            </pre>
+          {{content.sharingStateBottomUpChildToGrandpa}}
+        </pre>
       </div>
     </div>
 
@@ -203,56 +462,23 @@ import Child from "@/Child.vue";
       <div class="col-sm-12 col-md-4">
         <h4>Method Handlers</h4>
         <pre class="pre">
-&lt;template>
-  // Inline
-  &lt;button @click="counter++">Add 1&lt;/button>
-  &lt;p>The button above has been clicked { { counter }} times.&lt;/p>
-
-  // Function
-  &lt;button @click="greet">Greet&lt;/button>
-  &lt;button @click="say('hello')">Say Hello&lt;/button>
-&lt;/template>
-
-&lt;script setup>
-import { ref } from 'vue';
-
-const counter = ref(0);
-
-function greet(event) {
-  // event is the native DOM event
-  console.log(event.target.tagName);
-}
-
-function say(message) {
-  console.log(message);
-}
-&lt;/script>
-            </pre>
+          {{content.methodHandlers}}
+        </pre>
       </div>
 
       <div class="col-sm-12 col-md-4">
         <h4>Event Modifiers</h4>
         <pre class="pre">
-&lt;template>
-  &lt;a @click.stop="doThis">&lt;/a>
-  &lt;a @click.stop.prevent="doThat">&lt;/a>
-  &lt;div @click.self="myFunction">...&lt;/div>
-  &lt;form @submit.prevent="onSubmit">&lt;/form>
-&lt;/template>
-            </pre>
+          {{content.methodEventModifiers}}
+        </pre>
       </div>
 
       <div class="col-sm-12 col-md-4">
         <h4>Key Modifiers</h4>
         <pre class="pre">
-&lt;template>
-  &lt;input @keyup.enter="submit" />
-  &lt;input @keyup.page-down="onPageDown" />
-  &lt;input @keyup.delete="onDelete" /> // captures both "Delete" and "Backspace" keys
-&lt;/template>
-            </pre>
+          {{content.methodKeyModifiers}}
+        </pre>
       </div>
-
     </div>
 
     <div class="row">
@@ -262,33 +488,21 @@ function say(message) {
           <div class="col-sm-12 col-md-4">
             <h4>v-if</h4>
             <pre class="pre">
-&lt;template v-if="ok">
-  &lt;h1>Title&lt;/h1>
-  &lt;p>Paragraph 1&lt;/p>
-  &lt;p>Paragraph 2&lt;/p>
-&lt;/template>
-        </pre>
+              {{content.conditionalVif}}
+            </pre>
           </div>
 
           <div class="col-sm-12 col-md-4">
             <h4>v-else-if</h4>
             <pre class="pre">
-&lt;template>
-  &lt;div v-if="type === 'A'">A&lt;/div>
-  &lt;div v-else-if="type === 'B'">B&lt;/div>
-  &lt;div v-else>Else&lt;/div>
-&lt;/template>
+              {{content.conditionalVifElse}}
             </pre>
           </div>
 
           <div class="col-sm-12 col-md-4">
             <h4>v-show</h4>
             <pre class="pre">
-&lt;template>
-  &lt;h1 v-show="ok">Hello!&lt;/h1>
-&lt;/template>
-
-The v-show always renders in the DOM just toggling the CSS.
+              {{content.conditionalVshow}}
             </pre>
           </div>
         </div>
@@ -302,57 +516,22 @@ The v-show always renders in the DOM just toggling the CSS.
       <div class="col-sm-12 col-md-4">
         <h4>v-for on Array</h4>
         <pre class="pre">
-&lt;template>
-  &lt;li v-for="(item, index) in items" :key="item.message">
-    { { item.message }}
-  &lt;/li>
-&lt;/template>
-
-&lt;script setup>
-import { ref } from 'vue';
-
-const items = ref([
-  { message: 'Foo' },
-  { message: 'Bar' },
-]);
-&lt;/script>
-            </pre>
+          {{content.listVforArray}}
+        </pre>
       </div>
 
       <div class="col-sm-12 col-md-4">
         <h4>v-for on Object</h4>
         <pre class="pre">
-&lt;template>
-   &lt;ul>
-    &lt;li v-for="(value, key, index) in myObject" :key="index">
-      { { index }}. { { key }}: { { value }}
-      // 0. title: My Title
-      // 1. author: My Author
-    &lt;/li>
-  &lt;/ul>
-&lt;/template>
-
-&lt;script setup>
-import { reactive } from 'vue';
-
-const myObject = reactive({
-  title: 'My Title',
-  author: 'My Author',
-});
-&lt;/script>
-            </pre>
+          {{content.listVforObject}}
+        </pre>
       </div>
 
       <div class="col-sm-12 col-md-4">
         <h4>v-for on Component</h4>
         <pre class="pre">
-&lt;my-component
-  v-for="(item, index) in items"
-  :item="item"
-  :index="index"
-  :key="item.id"
-/>
-            </pre>
+          {{content.listVforComponent}}
+        </pre>
       </div>
 
     </div>
@@ -364,20 +543,8 @@ const myObject = reactive({
       <div class="col-sm-12 col-md-4">
         <h4>Accessing</h4>
         <pre class="pre">
-&lt;template>
-  &lt;input ref="input" />
-&lt;/template>
-
-&lt;script setup>
-import { ref, onMounted } from 'vue';
-
-const input = ref(null);
-
-onMounted(() => {
-  input.value.focus();
-});
-&lt;/script>
-            </pre>
+          {{content.templateRefsAccessing}}
+        </pre>
       </div>
     </div>
 
@@ -387,165 +554,47 @@ onMounted(() => {
       <div class="col-sm-12 col-md-6">
         <h4>ref()</h4>
         <pre class="pre">
-// Use for scalar types
-
-&lt;script setup>
-import {ref} from 'vue';
-
-const title = ref('Title');
-const price = ref(9.99);
-
-console.log(title.value);
-console.log(price.value);
-&lt;/script>
-            </pre>
+          {{content.reactivityRef}}
+        </pre>
       </div>
 
       <div class="col-sm-12 col-md-6">
         <h4>reactive()</h4>
         <pre class="pre">
-// Use for compound types
-
-&lt;script setup>
-import {reactive} from 'vue';
-
-const product = reactive({
-  title: 'Title',
-  price: 9.99
-});
-
-console.log(product.title);
-&lt;/script>
-            </pre>
+          {{content.reactivityReactive}}
+        </pre>
       </div>
 
       <div class="col-sm-12 col-md-6">
         <h4>ref(): Reactive State</h4>
         <pre class="pre">
-&lt;template>
-  &lt;button @click="increment">
-    { { count }}
-  &lt;/button>
-&lt;/template>
-
-&lt;script>
-import { ref } from 'vue';
-
-const count = ref(0)
-
-function increment() {
-  count.value++
-}
-&lt;/script>
-            </pre>
+          {{content.reactivityRefState}}
+        </pre>
       </div>
 
       <div class="col-sm-12 col-md-6">
         <h4>reactive(): Reactive State</h4>
         <pre class="pre">
-&lt;template>
-  &lt;button @click="increment">
-    { { state.count }}
-  &lt;/button>
-&lt;/template>
-
-&lt;script setup>
-import { reactive } from 'vue';
-
-const state = reactive({ count: 0 });
-
-function increment() {
-  state.count++
-};
-&lt;/script>
-            </pre>
+          {{content.reactivityReactiveState}}
+        </pre>
       </div>
     </div>
 
     <div class="row">
       <h3>Routing</h3>
-      <div class="col-sm-12 col-md-4">
+      <div class="col-sm-12 col-md-6">
         <h4>Simple Routing from Scratch</h4>
         <pre class="pre">
-&lt;template>
-  &lt;a href="#/">Home&lt;/a>
-  &lt;a href="#/about">About&lt;/a>
-  &lt;a href="#/non-existent-path">Broken Link&lt;/a>
-  &lt;component :is="currentView" />
-&lt;/template>
-
-&lt;script setup>
-import { ref, computed } from 'vue';
-import { createRouter, createWebHistory } from "vue-router";
-import Home from './Home.vue';
-import About from './About.vue';
-import NotFound from './NotFound.vue';
-
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home,
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: About,
-    },
-    {
-      path: "/not-found",
-      name: "notFound",
-      component: NotFound,
-    },
-  ],
-});
-
-export default router;
-
-const currentPath = ref(window.location.hash);
-
-window.addEventListener('hashchange', () => {
-  currentPath.value = window.location.hash
-});
-
-const currentView = computed(() => {
-  return routes[currentPath.value.slice(1) || '/'] || NotFound
-});
-
-&lt;/script>
-
-==========
-App.vue
-
-&lt;template>
-  &lt;nav>
-    &lt;RouterLink to="/home">Home&lt;/RouterLink>
-    &lt;RouterLink to="/about">About&lt;/RouterLink>
-    &lt;RouterLink to="/not-found">NotFound&lt;/RouterLink>
-  &lt;/nav>
-&lt;/template>
-
-&lt;script setup>
-import { RouterLink, RouterView } from "vue-router";
-&lt;/script>
-            </pre>
+          {{content.routing}}
+        </pre>
       </div>
 
-      <div class="col-sm-12 col-md-4">
+      <div class="col-sm-12 col-md-6">
         <h4>Redirecting</h4>
         <pre class="pre">
-&lt;template>&lt;/template>
-
-&lt;script setup>
-created() {
-  this.$router.push('/my-path');
-}
-&lt;/script>
-            </pre>
+          {{content.redirecting}}
+        </pre>
       </div>
-
     </div>
   </div>
 </template>
