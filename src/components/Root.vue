5a1c0e07297b0753a4fa368f47e04f1284b94144
Juliana Cochenski <juliana.cochenski@gmail.com>
2023-03-24 15:11:46 -0300

    Add about us

--- src/components/Root.vue
+++ src/components/Root.vue
@@ -1,5 +1,26 @@
 <template>
   <section class="container content">
     <h1 class="welcome">Hello! Click on the language you want to review.</h1>
+    <div class=aboutUs__headline>
+      <div class=aboutUs__headlineProfiles>
+        <div class="aboutUs__block">
+          <img class="aboutUs__block_border--img" src="src/assets/about-us__cesar.jpg" alt="cesar">
+          <p class="aboutUs__block_border--subtitle">Cesar Kohl</p>
+        </div>
+        <div class="aboutUs__block">
+          <img class="aboutUs__block_border--img" src="src/assets/about-us__julie.jpg" alt="julie">
+          <p class="aboutUs__block_border--subtitle">Julie Kohl</p>
+        </div>
+      </div>
+      <div>
+        <h2>About us</h2>
+        <h3>We are a couple of developers who value accessible, fast, beautiful, and innovative tech.</h3>
+        <p>
+          We are a couple of developers who love technology and the challenge of building something new, beautiful, functional, and that fits perfectly the needs of each project.<br>
+          We have over 10 years of experience counting dozens of satisfied clients. Our greatest achievement so far is having initiated a startup, making it profitable and selling it to a bigger player.<br>
+          We value accessible, fast, beautiful, and innovative websites. We are committed to work closely with our clients meeting agreed deadlines.
+        </p>
+      </div>
+    </div>
   </section>
 </template>
